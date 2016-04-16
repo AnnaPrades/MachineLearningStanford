379476c1cdcf0e8bb1f12d3b30929fff129c92ab
AnnaPrades <anna.prades@gmail.com>
2016-04-16 17:23:14 +0200

    Load Ex1 Machine Learning Stanford
    Load exercices Machine Learning Stanford. Coursera.

---
+++ Ex1 LinearRegression/computeCostMulti.m
@@ -0,0 +1,35 @@
+function J = computeCostMulti(X, y, theta)
+%COMPUTECOSTMULTI Compute cost for linear regression with multiple variables
+%   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+%1- Compute a vector h -> h(x) = theta T * x = theta0 + theta*x1
+    h = X * theta;
+
+    %2 -Compute the difference between the hypothesis and y
+    error = h - y;
+
+    %3- Compute the square of each of those error terms (using element-wise exponentiation)
+    error_sq = error.^2;
+
+    % sum of the error_sqr vector, and scale the result (multiply) by 1/(2*m)
+    J = 1/(2*m) * sum(error_sq);
+
+    
+    % With one line: J = sum((X * theta - y).^2) / (2 * m);
+
+
+
+% =========================================================================
+
+end
---
+++ Ex1 LinearRegression/gradientDescentMulti.m
@@ -0,0 +1,44 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCostMulti) and gradient here.
+    %
+    % 1 - hypothesis vector
+    h = X * theta;
+    % 2- errors vector
+    error = h - y;
+    % 3- theta_change vector
+    %he change in theta (the "gradient") is the sum of the product of X
+    %and the "errors vector", scaled by alpha and 1/m
+    % Since X is (m x n), and the error vector is (m x 1),and you want
+    % theta(2*1 or n *1) -> you need to transpose X
+    %vector multiplication authomaticaly sums the products
+    theta_change = alpha * (1/m) * (X'*error);
+    %4- Subtract this "change in theta" from the original value of theta.
+    theta = theta - theta_change;
+
+
+
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCostMulti(X, y, theta);
+
+end
+
+end
